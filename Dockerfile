a564fa26f7ddb63230aa25c9ff93944969d512bb
Matheus Lima <limamatheus0010@gmail.com>
2026-02-27 02:59:20 -0300

    Feat: Adicao do arquivo de configuracao do docker (DockerFile)

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin:17-jdk-alpine
+EXPOSE 80
+RUN mkdir /app
+COPY build/libs/ws-0.0.1-SNAPSHOT.jar /app/app.jar
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/cursochat/ws/handler/MessagePayload.java
+++ src/main/java/com/cursochat/ws/handler/MessagePayload.java
@@ -1,8 +1,5 @@
 package com.cursochat.ws.handler;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record MessagePayload(String to, String text) {
 }
